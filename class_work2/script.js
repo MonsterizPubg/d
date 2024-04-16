58e5a1b79ebee41af648a0d602c5bc3b40cc1b1d
Ilya <165833140+MonsterizPubg@users.noreply.github.com>
2024-04-16 20:06:04 +0500

    Add files via upload

---
+++ class_work2/script.js
@@ -0,0 +1,7 @@
+for (let i = 5; i >= 1; i--) {
+    for (let k = 1; k <= i; k++) {
+        document.write('*');
+    }
+    document.write('<br/>');
+}
+// я минусы по приколу поставил и сработало )
